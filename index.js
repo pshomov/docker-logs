78be21ead58471da6eeccbe5a9aa0316128b4c2f
Petar Shomov <petar@kolibri.is>
2014-12-24 15:47:34 -0500

    ref

--- index.js
+++ index.js
@@ -12,20 +12,22 @@ var docker = new Docker({
     port: 4243
 });
 var emitter = new DockerEvents({
-  docker: docker,
+    docker: docker,
 });
 
-function containerName(containerInfo){
-	return _(containerInfo.Names).filter(function(name){return name.split('/').length == 2})[0].split('/')[1];
+function containerName(containerInfo) {
+    return _(containerInfo.Names).filter(function(name) {
+        return name.split('/').length == 2
+    })[0].split('/')[1];
 }
 
-q.ninvoke(docker,'listContainers')
+q.ninvoke(docker, 'listContainers')
     .then(function(containersInfo) {
         return q.all(_(containersInfo)
             .map(function(containerInfo) {
-            	var name = containerName(containerInfo);
-            	var container = docker.getContainer(containerInfo.Id);
-                return q.ninvoke(container,'logs',{
+                var name = containerName(containerInfo);
+                var container = docker.getContainer(containerInfo.Id);
+                return q.ninvoke(container, 'logs', {
                         follow: true,
                         timestamps: true,
                         stdout: true,
@@ -51,33 +53,33 @@ q.ninvoke(docker,'listContainers')
 
 emitter.start();
 emitter.on("connect", function() {
-  console.log("connected to docker api");
+    console.log("connected to docker api");
 });
 
 emitter.on("disconnect", function() {
-  console.log("disconnected to docker api; reconnecting");
+    console.log("disconnected to docker api; reconnecting");
 });
 
 emitter.on("_message", function(message) {
-  console.log("got a message from docker: %j", message);
+    console.log("got a message from docker: %j", message);
 });
 
 emitter.on("create", function(message) {
-  console.log("container created: %j", message);
+    console.log("container created: %j", message);
 });
 
 emitter.on("start", function(message) {
-  console.log("container started: %j", message);
+    console.log("container started: %j", message);
 });
 
 emitter.on("stop", function(message) {
-  console.log("container stopped: %j", message);
+    console.log("container stopped: %j", message);
 });
 
 emitter.on("die", function(message) {
-  console.log("container died: %j", message);
+    console.log("container died: %j", message);
 });
 
 emitter.on("destroy", function(message) {
-  console.log("container destroyed: %j", message);
+    console.log("container destroyed: %j", message);
 });
